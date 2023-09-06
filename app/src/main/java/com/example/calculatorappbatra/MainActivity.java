01dbfe250aecedd9f68602db3f85a0579d1b9b2d
ish <93941745+ishies@users.noreply.github.com>
2023-09-06 12:54:20 -0500

    Made home page, add button layout PLEASE !!

--- app/src/main/java/com/example/calculatorappbatra/MainActivity.java
+++ app/src/main/java/com/example/calculatorappbatra/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.calculatorappbatra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public void buttonPage (View v) {
+        Intent intent = new Intent(this, ButtonPage.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
